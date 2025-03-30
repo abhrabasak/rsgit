d0479895384ef79353ad7ba1f48209e4c06958a0
Abhra Basak <abhra@outlook.in>
2025-03-30 00:59:40 -0700

    Implement git-init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::fs;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+struct Cli {
+    /// The command to run
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand, Debug)]
+enum Command {
+    Init,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    match &args.command {
+        Command::Init => init(),
+    }
+}
+
+fn init() {
+    fs::create_dir(".git").unwrap();
+    fs::create_dir(".git/objects").unwrap();
+    fs::create_dir(".git/refs").unwrap();
+    fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
+    println!("Initialized git directory");
 }
